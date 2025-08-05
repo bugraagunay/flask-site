80605c36ff99b202c8df8fcde02b7f63eefc8064
bugraagunay <bugragunay4@gmail.com>
2025-08-05 19:41:31 +0300

    Add all project files

--- debug_turkey_data.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-import pandas as pd
-
-conn = sqlite3.connect('income.db')
-df = pd.read_sql_query("SELECT * FROM income_data WHERE Country = 'Türkiye' AND Year = 2022", conn)
-conn.close()
-
-print(df.to_string())
